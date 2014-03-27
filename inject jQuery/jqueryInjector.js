d63fa05fad7b6f92e12c0ca27460996f282bf3df
Johnnie Blevins <johnnie.blevins@blackbaud.com>
2014-03-27 13:19:14 -0500

    updated list message, added jquery injector

---
+++ inject jQuery/jqueryInjector.js
@@ -0,0 +1,9 @@
+!function(){
+	var b = document.body;
+	if (b) {
+	    var z = document.createElement('script');
+	    z.type = 'text/javascript';
+	    z.src = '//code.jquery.com/jquery-1.11.0.min.js';
+	    b.appendChild(z);
+	}
+}()
--- perusable/src/perusable.js
+++ perusable/src/perusable.js
@@ -9,9 +9,9 @@
 		,infoBox=document.createElement('div')
 		,node
 		,reusables=[]
-		,msg=document.createTextNode("Not in reusable")
+		,msg=document.createTextNode('Not in reusable')
 		,update=function(e){
-			msg.nodeValue=this.hasAttribute('from_reusable') ? this.getAttribute('from_reusable') : "Not in a reusable";
+			msg.nodeValue=this.hasAttribute('from_reusable') ? this.getAttribute('from_reusable') : 'Not in a reusable';
 			infoBox.style.left='0px';
 			infoBox.style.top='0px';
 			infoBox.style.visibility='hidden';
@@ -25,7 +25,7 @@
 		},
 		log=function(e){
 			if (e.shiftKey){
-				console.log("Current reusable path is "+msg.nodeValue);
+				console.log('Current reusable path is '+msg.nodeValue);
 			}
 			e.stopPropagation();
 			return false;
@@ -50,21 +50,21 @@
 		}
 	}
 	if (reusables.length){
-		infoBox.innerHTML='<div style="border-radius:8px;box-shadow:2px 2px 4px rgba(0,0,0,.7);white-space:nowrap;z-index:99999;padding:2px 5px;position:fixed; background:white; border: 1px solid black; width:auto; top:0;left:0;min-width: 60px;"></div>';
+		infoBox.innerHTML='<div style=&quot;border-radius:8px;box-shadow:2px 2px 4px rgba(0,0,0,.7);white-space:nowrap;z-index:99999;padding:2px 5px;position:fixed; background:white; border: 1px solid black; width:auto; top:0;left:0;min-width: 60px;&quot;></div>';
 		infoBox=infoBox.firstChild;
 		infoBox.appendChild(msg);
 		document.body.appendChild(infoBox);
 		document.body.addEventListener('mouseover',update);
-		alert("The following reusables are present on this page:\n"+reusables.join('\n'));
-		console.log("The following reusables are present on this page:\n(These are also logged to the console)\n"+reusables.join('\n')+"\nShift click on an element to log its closest reusable to the console.");
+		alert('The following reusables are present on this page:\nThese are also logged to the console)\n'+reusables.join('\n'));
+		console.log('The following reusables are present on this page:\n('+reusables.join('\n')+'\nShift click on an element to log its closest reusable to the console.');
 	} else {
 		infoBox=node=update=msg=walker=reus=reusables=undefined;
 		if (window.location.href.search('s_debug=true')==-1){
-			if(confirm("No reusables were found. Reload with s_debug=true?\n(You'll have to click the bookmarklet again afterward)")){
+			if(confirm('No reusables were found. Reload with s_debug=true?\n(You will have to click the bookmarklet again afterward)')){
 				window.location=window.location.href.replace(/\?|(#|$)/,'?s_debug=true&$1')
 			}
 		} else {
-			alert("No reusables were found on the page.");
+			alert('No reusables were found on the page.');
 		}
 	}
 }();
